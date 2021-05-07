a5ae43185ade6a350e9d5112506f98d2ff26d583
Carl <cfuer12@yahoo.com>
2021-05-06 21:39:48 -0700

    added footer

---
+++ client/src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer text-center container-fluid font-small text-light">
+      <span>© 2021 Copyright Gentle Drinkers</span>
+    </footer>
+  );
+}
+
+export default Footer;
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Footer from '../components/Footer/footer'
 import Navbar from "../components/NavBar/navbar"
 
 const Main = () => {
     return (
         <div>
             <Navbar />
+            <Footer />
         </div>
     )
 }
