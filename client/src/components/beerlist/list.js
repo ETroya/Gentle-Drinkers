872c6def7dba6f241973be48cc0de8f7b3e875df
Carl <cfuer12@yahoo.com>
2021-05-13 21:27:12 -0700

    added beerlist to components

---
+++ client/src/components/beerlist/list.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./list.css";
+
+function List() {
+    return (
+        <div>
+            <Container>
+                <table id="list" container="table"></table>
+            </Container>
+        </div>
+    );
+};
+
+export default List;
+
